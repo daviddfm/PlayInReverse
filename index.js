763029e1a592cd1491fed9e9c9329dfce3278bc8
daviddfm <david@dawson.fm>
2015-06-17 19:15:04 -0400

    fix

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', "index.html");
+app.use(express.static(__dirname));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
