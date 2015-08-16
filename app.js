858732d6a7e4275313da207be7ed566235351dad
daviddfm <david@dawson.fm>
2015-08-16 12:55:16 +0800

    try offset & limit

--- app.js
+++ app.js
@@ -261,7 +261,7 @@ function getPlaylists(username, callback) {
 
 function getTracksForPlaylist(username, playlist, callback) {
 	console.log('getTracksForPlaylist', username, playlist);
-	var url = 'https://api.spotify.com/v1/users/' + username + '/playlists/' + playlist + '/tracks?limit=140';
+	var url = 'https://api.spotify.com/v1/users/' + username + '/playlists/' + playlist + '/tracks?offset=0&limit=200';
 		
 	$.ajax(url, {
 		method: 'GET',
